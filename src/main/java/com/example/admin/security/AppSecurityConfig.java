e5ccdc706520b8e057864e2a87a4716118becdfb
Sempo <younessbouhjar55@gmail.com>
2022-02-09 13:37:55 +0100

    a

--- src/main/java/com/example/admin/security/AppSecurityConfig.java
+++ src/main/java/com/example/admin/security/AppSecurityConfig.java
@@ -100,13 +100,14 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 				"origin",
 				"user-agent",
 				"x-csrftoken",
-				"x-requested-with"));
+				"x-requested-with",
+				"Access-Control-Allow-Origin"));
 		// setAllowCredentials(true) is important, otherwise:
 		// The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
 		configuration.setAllowCredentials(true);
 		// setAllowedHeaders is important! Without it, OPTIONS preflight request
 		// will fail with 403 Invalid CORS request
-		configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+		configuration.setAllowedHeaders(ImmutableList.of("Authorization","Access-Control-Allow-Origin" ,"Cache-Control", "Content-Type"));
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
 		return source;
